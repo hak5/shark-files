a42de27a1191531e5622f53681935b6c080933fc
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2020-02-26 17:05:43 -0500

    put helpful things in an extension

---
+++ root/payload/extensions/HAK5_HELPERS.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# HAK5_HELPERS by @Zero_ChaosX
+# Built in helper functions to aid writing payloads
+
+function find_subnet() {
+    #USAGE:  find_subnet
+    #Result: defines SUBNET environment variable, if an IP address has been assigned
+    #Notes:  this function only finds ipv4 subnets but is safe in the presence of ipv6
+    #        removing the tail -n1 would make this a newline seperated list of assigned ip addresses
+    SUBNET=$(ip -4 addr show eth0 | awk '/inet\s/ {print $2}' | sed 's/\.[0-9]*\//\.0\//' | tail -n1)
+    export SUBNET
+}
+export -f find_subnet
+
+#function wait_for_link()
+    #USAGE:  wait_for_link
+    #Result: execution pauses until link connected state is detected
+    #Notes:  defined in /usr/bin/execute_payload
+
+wait_for_no_link() {
+    #USAGE:  wait_for_no_link
+    #Result: execution pauses until link disconnected state is detected
+    LED LINKSETUP
+    until swconfig dev switch0 port 0 get link | grep -q 'link:down'; do
+        sleep 1
+    done
+    LED SETUP
+}
+export -f wait_for_no_link
